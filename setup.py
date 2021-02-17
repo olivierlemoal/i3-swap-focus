e855caed6fd7273fb4d9642cf55586c21ddac729
Olivier Le Moal <olivier.lemoal@nbs-system.com>
2021-02-17 18:35:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='i3-swap-focus',
+    version='0.1',
+    description='i3 script to toggle between last windows',
+    packages=find_packages(),
+    author='Olivier Le Moal',
+    author_email='mail@olivierlemoal.fr',
+    url='https://github.com/olivierlemoal/i3-swap-focus',
+    license='MIT',
+    platforms=['any'],
+    install_requires=['i3ipc'],
+    scripts=['i3_swap_focus'],
+)
