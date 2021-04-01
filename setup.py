e6433ce9db927aaeea13cd19389ffe485960aae8
Olivier Le Moal <olivier.lemoal@p1sec.com>
2021-04-01 23:24:18 +0200

    Update to 0.3

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='i3-swap-focus',
-    version='0.2',
-    description='i3 script to toggle between last windows',
+    version='0.3',
+    description='i3/sway script to toggle between last windows',
     packages=find_packages(),
     author='Olivier Le Moal',
     author_email='mail@olivierlemoal.fr',
